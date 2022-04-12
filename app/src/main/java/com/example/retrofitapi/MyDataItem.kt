8b6de748e7293e25ba91d053c403d8c2363d8d14
administrator <an-vishal@indicchain.com>
2022-04-12 13:41:20 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/retrofitapi/MyDataItem.kt
@@ -0,0 +1,10 @@
+package com.example.retrofitapi
+
+import com.google.gson.annotations.SerializedName
+
+data class MyDataItem(
+    val body: String,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
